51d977928fe8901bc232cf8d4832bd361614f7b1
tarek <tarekzakitdkr@gmail.com>
2024-01-23 05:01:58 +0200

    modified chat

--- store/chat/models.py
+++ store/chat/models.py
@@ -2,15 +2,10 @@ from django.db import models
 
 from user.models import User
 
-"""
+
 class Chat(models.Model):
-    particepent_1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, blank=False, null=False, related_name="userOne"
-    )
-    particepent_2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, blank=False, null=False, related_name="userTwo"
-    )
-"""
+	pass
+
 
 
 class Message(models.Model):
@@ -22,6 +17,8 @@ class Message(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     body = models.CharField(max_length=500, blank=False, null=False)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, blank= False, null=False)
+    read = models.BooleanField(default = False)
 
     def get_messages(pk1, pk2):
         messages = []
